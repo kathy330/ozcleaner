fcb013d918542a3175f56b45940701ca795ba7ec
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-02-06 20:34:40 +1000

    12345

--- src/pages/Mengxuan/Mengxuan.js
+++ src/pages/Mengxuan/Mengxuan.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MengxuanComponent from '../../component/MengxuanComponent'
+import MengxuanComponent from '../../component/Mengxuan/MengxuanComponent'
 
 function MengxuanPage (){
     return <MengxuanComponent />

ad658fdb1d735243d5848b79339c8b0dffe04183
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-02-06 16:54:15 +1000

    1234

--- src/component/KathyComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const KathyComponent = () => <h1>Kathy&apos;s Page</h1>;
-
-export default KathyComponent
--- src/component/MengxuanComponent.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-
-const MengxuanComponent=()=> <div>This is MengxuanComponent</div>
-
-export default MengxuanComponent;

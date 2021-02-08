8ad23c62310762cfa23d57ee66671f9b93170a24
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-02-08 10:37:43 +1000

    12345

--- src/components/Wystan/WystanComponent.js
+++ src/components/Wystan/WystanComponent.js
@@ -1,15 +1,38 @@
+import axios from 'axios';
 import React  from 'react';
+import Api from '../../api/Wystan/WystanApi'
 
 class WystanComponent extends React.Component {
     constructor() {
         super();
         this.state = {
+            image:''
+        }
+    }
+    
+    componentDidMount() {
+        this.getApi();
+    }
 
+    getApi = async() => {
+        try {
+            const response = await axios.get(Api())
+            console.log(response)
+            const image = response.data.message;
+            console.log(image);
+            this.setState({
+                image:image
+            })
+            console.log(image)
+        } catch (e) {
+            console.log(e)
         }
     }
 
     render() {
-        return <div> This is built from wystan</div>
+        return <div> This is built from wystan
+            <img src = {this.state.image} alt = "dog" />
+        </div>
     }
 }
 

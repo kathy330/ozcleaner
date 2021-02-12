8a04d047cc3c891ad99f40f2fc1e2212a6dbc3c3
Kathy <katekathywu@gmail.com>
2021-02-12 22:06:50 +1100

    connect to backend

--- src/api/Kathy/KathyApi.js
+++ src/api/Kathy/KathyApi.js
@@ -1,4 +1,6 @@
 const KathyKey = process.env.REACT_APP_KATHY_KEY;
 
 const getWebApi = () => `https://dog.ceo/api/breeds/image/${KathyKey}`;
-export default getWebApi;
+const getBackendApi = () => "http://localhost:8000/kathy";
+
+export { getWebApi , getBackendApi};

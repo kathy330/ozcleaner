c2ef0f442ba2ae3842c6c1fcdccdf9954294c5d9
Mengxuan <mengxuanzhangvip@gmail.com>
2021-02-12 22:44:13 +1000

    updated api

--- src/api/Mengxuan/MengxuanApi.js
+++ src/api/Mengxuan/MengxuanApi.js
@@ -1,4 +1,6 @@
-// const Mengxuan_key = process.env.REACT_APP_MENGXUAN_KEY;
-const getWebApi = () => "https://dog.ceo/api/breeds/image/random";
+const Mengxuankey = process.env.REACT_APP_MENGXUAN_KEY;
+const getWebApi = () => `https://dog.ceo/api/breeds/image/${Mengxuankey}`;
+const getBackendApi = () => "http://localhost:8000/mx";
 
-export default getWebApi;
+
+export {getWebApi,getBackendApi}

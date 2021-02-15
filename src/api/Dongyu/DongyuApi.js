d127c786074e9a42d73892561efb4bacf953a17f
Vincent wu <vincent997799@gmail.com>
2021-02-16 00:59:37 +1000

    -

--- src/api/Dongyu/DongyuApi.js
+++ src/api/Dongyu/DongyuApi.js
@@ -3,9 +3,9 @@ const DongyuKey = process.env.REACT_APP_DONGYU_KEY
 const getWebApi = () => `https://dog.ceo/api/breeds/image/${DongyuKey}`
 const getBackendApi1 = () => "http://localhost:8000/dy/users"
 const getBackendApi2 = () => "http://localhost:8000/dy/book"
-const getUserListApi = () => "http://localhost:8000/userList"
-const getRegRoomApi = () => "http://localhost:8000/regular"
-const getEndRoomApi = () => "http://localhost:8000/endOfLease"
+const getUserListApi = () => "http://localhost:8000/dy/userList"
+const getRegRoomApi = () => "http://localhost:8000/dy/regular"
+const getEndRoomApi = () => "http://localhost:8000/dy/endOfLease" 
 
 
 export  {getWebApi,getBackendApi1,getBackendApi2,getUserListApi,getRegRoomApi,getEndRoomApi}

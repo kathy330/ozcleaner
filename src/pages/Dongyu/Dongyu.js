fe0b8d0110b003f048f9e6d0fa65b9dd32200163
Vincent <415578597@qq.com>
2021-02-21 16:10:22 +1000

    update order status,+1

--- src/pages/Dongyu/Dongyu.js
+++ src/pages/Dongyu/Dongyu.js
@@ -1,6 +1,7 @@
 import React from "react"
 import DongyuComponent from "../../components/Dongyu/DongyuComponent"
 import InsertOrder from "../../components/Dongyu/InsertOrder"
+import FinishOrder from "../../components/Dongyu/FinishOrder"
 import "./Dongyu.scss"
 
 function DongyuPage(){
@@ -8,6 +9,7 @@ function DongyuPage(){
       <div className="dongyu-page">
         {/* <h1>This is Dongyu&apos;s page</h1> */}
         <InsertOrder />
+        <FinishOrder />
         <DongyuComponent />
       </div>
     )

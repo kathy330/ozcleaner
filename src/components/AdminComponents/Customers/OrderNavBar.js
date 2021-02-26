2b02b0b1d36a69514dbe0010c882123f173ff91a
Vincent <415578597@qq.com>
2021-02-26 21:51:59 +1000

    🎉 admin Customer page

---
+++ src/components/AdminComponents/Customers/OrderNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import styles from './scss/HomeTitle.module.scss'
+
+
+const OrderNavBar = () => (
+  <div>
+    <button type="submit">unconfirmed</button>
+    <button type="submit">in progress</button>
+    <button type="submit">unconfirmed</button>
+    <button type="submit">unconfirmed</button>
+  </div>
+)
+
+export default OrderNavBar

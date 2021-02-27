b703a79f5a1b04fcbdfb4b845c686326737a2ad5
liyanbo1995 <liyanbo1995@hotmail.com>
2021-02-27 15:24:44 +1000

    added dialog testing

--- src/components/HomeComponents/HomeSelectForm.js
+++ src/components/HomeComponents/HomeSelectForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import styles from './scss/HomeSelectForm.module.scss'
-
+import styles from './scss/HomeSelectForm.module.scss' 
 
 class HomeSelectForm extends React.Component {
 
--- src/components/zpractice/Yanbo/PopupForm.js
+++ src/components/zpractice/Yanbo/PopupForm.js
@@ -4,9 +4,9 @@ import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
+// import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
-
+import { makeStyles } from '@material-ui/core/styles'
 
 export default function FormDialog() {
     const [open, setOpen] = React.useState(false)
@@ -18,35 +18,60 @@ export default function FormDialog() {
     const handleClose = () => {
       setOpen(false)
     }
+    const useStyles = makeStyles({
+        root: {
+          // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          fontSize:'3.8rem',
+          fontWeight:'bold',
+          textAlign:'center',
+          borderRadius:60,
+          input:{
+            borderRadius:30
+        }
+        },
+        
+      })
+
+    const classes = useStyles()
   
     return (
+        
       <div>
         <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-          黑猫警长
+          黑猫警长向您致敬
         </Button>
+        
         <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogTitle className={classes.root}>Join us</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              To subscribe to this website, 
-              please enter your email address here. We will send updates
-              occasionally.
-            </DialogContentText>
+            <DialogTitle>Email</DialogTitle>
+            <TextField
+              className={classes.root}
+              autoFocus
+              margin="dense"
+              id="outlined-basic"
+              label="Email"
+              type="email"
+              fullWidth
+              variant="outlined"
+            />
+            <DialogTitle>Password</DialogTitle>
             <TextField
               autoFocus
               margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
+              id="outlined-basic"
+              label="Password"
+              type="password"
               fullWidth
+              variant="outlined"
             />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
-              Cancel
+              Login
             </Button>
             <Button onClick={handleClose} color="primary">
-              Subscribe
+              Registration
             </Button>
           </DialogActions>
         </Dialog>

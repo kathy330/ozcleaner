c4c2db4bbce139bb2815422af5989b93f5ed4476
Vincent <415578597@qq.com>
2021-02-27 15:35:09 +1000

    🔥 add globle color style

--- src/components/HomeComponents/components/DatePicker.js
+++ src/components/HomeComponents/components/DatePicker.js
@@ -1,8 +1,8 @@
 import 'date-fns'
 import React from 'react'
-// import Grid from '@material-ui/core/Grid'
 import DateFnsUtils from '@date-io/date-fns'
-import {MuiPickersUtilsProvider,KeyboardDatePicker} from '@material-ui/pickers'
+import {MuiPickersUtilsProvider,KeyboardDatePicker} from "@material-ui/pickers"
+
 import HomeComponentStyle from '../styles/HomeComponentStyle'
 import getTime from './getTime'
 
@@ -25,7 +25,6 @@ export default function MaterialUIPickers() {
       {/* <Grid container justify="space-around"> 日历框框周围间距 */}
       <KeyboardDatePicker
         // margin="normal"
-        // style={diyStyle.dateStyle}
         className={classes.datePicker}
         id="date-picker-dialog"
         label="Choose your date"

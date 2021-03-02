1a68ad10c05271c884bab1e53c5ee632a3070a48
Vincent <415578597@qq.com>
2021-03-03 01:36:25 +1000

    ✨准备导入h1-h8样式

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -33,10 +33,34 @@ const theme = createMuiTheme({
       },
     },
 
-    iconSize: {
-      
-    }
+    // 统一字体：
+    caption:{ // 首页最大标题（We clean)
 
+    },
+    subtitle:{ // 首页小标题（Get your)
+     
+    },
+    button:{
+      
+    },
+    h1: { // 首页（User feedback title/How we works）
+      
+    },
+
+    h2:{ // 首页（User feedback here）
+     
+    },
+
+    h3:{ // 首页（-User name）
+
+    },
+    h4:{
+
+    },
+    h5:{
+
+    },
+    
 
 })
 

8692ac8e53ef7219aac9c453b13baeac8e662cb5
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-03-03 16:23:12 +1000

    :tada: Admin Dashboard Page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ProfilePage from "./pages/ProfilePage/ProfilePage"
 import AdminCustomersPage from "./pages/AdminPage/AdminCustomersPage"
 import ErrorPage from "./pages/ErrorPage/ErrorPage"
 import theme from "./styles/theme"
+import AdminDashboardPage from "./pages/AdminPage/AdminDashboardPage"
 // import ....Page from ....
 
 import DongyuPage from "./pages/zpractice/Dongyu/Dongyu"
@@ -39,7 +40,7 @@ function App() {
         {/* <Route path="/admin/order" exact component={AdminOrderPage} />
         <Route path="/admin/overview" exact component={AdminOverviewPage} />
         <Route path="/admin/staff" exact component={AdminStaffPage} /> */}
-
+        <Route path="/admin/dashboard" exact component={AdminDashboardPage} />
         <Route path="/button" exact component={ButtonPage} />
 
 

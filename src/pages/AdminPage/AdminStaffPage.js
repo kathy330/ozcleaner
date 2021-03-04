a1952f793ec4b9f8dbf6b6784fed994a89d1fa5d
olivia shen <>
2021-03-04 23:33:45 +1100

    update staff March 4

--- src/pages/AdminPage/AdminStaffPage.js
+++ src/pages/AdminPage/AdminStaffPage.js
@@ -4,14 +4,16 @@ import Container from '@material-ui/core/Container'
 import { Box } from "@material-ui/core"
 import Display from "../../components/AdminComponents/Staffs/Display"
 import Table from "../../components/AdminComponents/Staffs/Table"
+import NavBar from "../../components/NavBarComponents/NavBar"
 
 function AdminStaffPage(){
   return (
-    
+   
     <Box bgcolor="#eaeaea">
-      <CssBaseline />
+      <NavBar />
+      <CssBaseline />      
       <Container maxWidth="md">
-        <Box bgcolor="white">
+        <Box bgcolor="white" marginTop="0.2%">
           <Display />         
         </Box>
         <Box>

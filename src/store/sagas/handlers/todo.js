c072307873e43537d15bbd0e480572d2d079b6a9
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-03-08 00:02:00 +1000

    :art:update the saga

---
+++ src/store/sagas/handlers/todo.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-fallthrough */
+/* eslint-disable import/prefer-default-export */
+
+import { call, put, select, fork } from 'redux-saga/effects'
+import axios from 'axios'
+import actionTypes from '../actions/actionTypes'
+import { requestOrderList } from '../requests/todo'
+
+
+export default function* asyncRequestOrders(action) {
+    switch (action.type) {
+        case actionTypes.LOAD_ORDER_REQUESTED: {
+            try {
+
+                // const { data } = yield call(axios.get, "http://localhost:8000/regular")
+                // console.log(requestOrderList)
+                const result = yield call(requestOrderList, "this is input")
+                // console.log(oneTodoRes)
+                yield put({ type: 'LOAD_ORDER_SUCCEEDED', data: result })
+            } catch (err) {
+                console.log(err)
+            }
+            break
+        }
+
+        default: {
+            console.log("this is default")
+        }
+    }
+}

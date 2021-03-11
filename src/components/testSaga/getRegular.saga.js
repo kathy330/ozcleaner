9f143c03deb60224f157c72eda95796d70e05717
Vincent <415578597@qq.com>
2021-03-11 23:02:37 +1000

    small change

---
+++ src/components/testSaga/getRegular.saga.js
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+
+
+import {getGitRequest} from '../../store/actions/index'
+
+
+const GitList = () => {
+
+  const dispatch = useDispatch()
+  const repo = useSelector(state => state.git_in_reducer_index.repos_in_reducer_init)
+  
+  useEffect(()=>{
+    dispatch(getGitRequest())
+  },[])
+  console.log(repo)
+
+  return(
+    <>
+      hi
+      <ul>
+        {repo.map((item)=>
+          // console.log(item)
+          (
+            <>
+              <h2>
+                <br />
+                {item.status}
+                <br />
+                {item.startTime}
+                <br />
+                {item.taskID}
+              </h2>
+            </>
+          )
+          )}
+      </ul>
+    </>
+  )
+}
+export default GitList

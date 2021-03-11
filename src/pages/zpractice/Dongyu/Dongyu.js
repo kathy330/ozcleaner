391bf93dad70dd0ddf984ebf0fc9077a9c5ca8a1
Vincent <415578597@qq.com>
2021-03-11 23:01:34 +1000

    small change

--- src/pages/zpractice/Dongyu/Dongyu.js
+++ src/pages/zpractice/Dongyu/Dongyu.js
@@ -1,15 +1,19 @@
 import React from "react"
 // import DongyuComponent from "../../../components/zpractice/Dongyu/DongyuComponent"
-import InsertOrder from "../../../components/zpractice/Dongyu/InsertOrder"
-import FinishOrder from "../../../components/zpractice/Dongyu/FinishOrder"
+// import InsertOrder from "../../../components/zpractice/Dongyu/InsertOrder"
+// import FinishOrder from "../../../components/zpractice/Dongyu/FinishOrder"
+import GetRegularSaga from "../../../components/testSaga/getRegular.saga"
 import "./Dongyu.scss"
 
 function DongyuPage(){
     return (
       <div className="dongyu-page">
         {/* <h1>This is Dongyu&apos;s page</h1> */}
-        <InsertOrder />
-        <FinishOrder />
+
+        <GetRegularSaga />
+        
+        {/* <InsertOrder />
+        <FinishOrder /> */}
         {/* <DongyuComponent /> */}
       </div>
     )

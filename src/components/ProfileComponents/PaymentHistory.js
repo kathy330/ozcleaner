3e930438f9ffef43035143606048ee2fc47cc1be
Mengxuan <mengxuanzhangvip@gmail.com>
2021-03-12 12:34:30 +1000

    :lipstick: add UI file

---
+++ src/components/ProfileComponents/PaymentHistory.js
@@ -0,0 +1,155 @@
+import React from "react"
+import { makeStyles} from "@material-ui/core/styles"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import Typography from "@material-ui/core/Typography"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Paper from "@material-ui/core/Paper"
+import Button from '@material-ui/core/Button'
+import TablePagination from '@material-ui/core/TablePagination'
+import KingBedIcon from '@material-ui/icons/KingBed'
+import BathtubIcon from '@material-ui/icons/Bathtub'
+
+const useStyles = makeStyles((theme) => ({
+  
+  table: {
+    minWidth: 500,
+    [theme.breakpoints.down((1550))]:{
+      minWidth: 400,
+    },
+    [theme.breakpoints.down((700))]:{
+      minWidth: 300,
+    },
+    [theme.breakpoints.down((420))]:{
+      minWidth: 100,
+    },
+
+  },
+  price:{
+    color:"#007bf5",
+  },
+  check: {
+    display:"inline-block",
+    background:"#007bf5",
+    color:"white"
+  },
+
+  fontSize: {
+    font:'25px',
+  },
+  orderNumber: {
+    margin:'0 1px 0 1px',
+    align:'center',
+    display:'inline'
+  },
+  bedIconSize:{
+    verticalAlign:'-4px'
+  },
+  bathIconSize:{
+    verticalAlign:'-2px'
+  },
+}
+))
+
+function createData(orderId, numOfBedrooms, numOfBathrooms, date, price, action) {
+  return { orderId, numOfBedrooms, numOfBathrooms, date, price, action }
+}
+
+const rows = [
+  createData("01", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("02", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("03", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("04", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("05", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("06", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("07", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("08", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("09", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("10", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("11", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("12", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("13", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("14", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("15", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("16", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("17", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("18", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("19", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("20", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("21", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("22", "1", "3","11:30AM 22 Jan 2021", "68.5","Check"),
+  createData("23", "1", "3","11:30AM 22 Jan 2021", "68.5","Check")
+]
+
+export default function BasicTable() {
+  const classes = useStyles()
+
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Order ID</TableCell>
+            <TableCell align="center">Details</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Price</TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => ( 
+            <TableRow key={row.orderId} vertical-align='middle'>
+              <TableCell align="center">{row.orderId}</TableCell>
+              <TableCell align='center'>
+                <KingBedIcon className={classes.bedIconSize} />
+                <Typography className={classes.orderNumber}>
+                  ×
+                  {row.numOfBedrooms}
+                </Typography>
+                <BathtubIcon className={classes.bathIconSize} />
+                <Typography className={classes.orderNumber}>
+                  ×
+                  {row.numOfBathrooms}
+                </Typography>
+              </TableCell>
+              <TableCell align="center">{row.date}</TableCell>
+              <TableCell align="center">
+                <Typography>{row.price}</Typography>
+              </TableCell>
+              <TableCell align="center">
+                <Button variant="contained" className={classes.check}>
+                  {row.action}
+                </Button>
+              </TableCell>
+            </TableRow>
+       ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+        align="center"
+      />
+    </TableContainer>
+  )
+}
---
+++ src/components/ProfileComponents/Profile.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import SwipeableViews from 'react-swipeable-views'
+import { makeStyles,  Grid, Typography, Tabs, Tab, useTheme  } from '@material-ui/core'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import scssStyle from './scss/Profile.module.scss'
+import ProfileRight from './ProfileRight'
+import ProfileLeft from './ProfileLeft'
+import PaymentHistory from './PaymentHistory'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+}))
+function TabContainer({ children, dir }) {
+    return (
+      <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+        {children}
+      </Typography>
+    )
+  }
+  
+  TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+    dir: PropTypes.string.isRequired,
+  }
+  
+export default function AutoGrid() {
+  const classes = useStyles()
+
+  const theme = useTheme()
+  const [value, setValue] = React.useState(0)
+
+  function handleChange(event, newValue) {
+    setValue(newValue)
+  }
+
+  function handleChangeIndex(index) {
+    setValue(index)
+  }
+  const mediumViewport = useMediaQuery('(min-width:600px)')
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={0} width='100vh'>
+        <Grid item md={2} className={scssStyle.background1} />
+        <Grid item md={2} sm={4} xs={12} className={scssStyle.background2}>
+          <Grid container md={12} item lg={12} justify="center" alignItems="center">
+            <ProfileLeft />
+          </Grid>
+          <Grid item xs={12} lg={12}>
+            <>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant="fullWidth"
+                orientation={mediumViewport ? "vertical" : "horizontal"}
+              > 
+                <Tab label="Profile" />
+                <Tab label="Payment Hitory" />
+              </Tabs>
+            </>
+          </Grid>
+        </Grid>
+        <Grid item md={6} sm={8} xs={12} className={scssStyle.background}>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabContainer
+              dir={theme.direction}
+              className={scssStyle.background}
+            >
+              <ProfileRight />
+            </TabContainer>
+            <TabContainer dir={theme.direction}>
+              <PaymentHistory />
+            </TabContainer>
+          </SwipeableViews>
+        </Grid>
+        <Grid item md={2} />
+      </Grid>
+    </div>
+  )
+}

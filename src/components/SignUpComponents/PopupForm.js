3cb7f43473e95a262b5cad898b2d704d5bf751a6
liyanbo1995 <liyanbo1995@hotmail.com>
2021-03-16 22:32:05 +1000

    solved conflict

--- src/components/SignUpComponents/PopupForm.js
+++ src/components/SignUpComponents/PopupForm.js
@@ -1,15 +1,62 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import Divider from '@material-ui/core/Divider'
-import {makeStyles} from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import { makeStyles ,useTheme } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 import Typography from '@material-ui/core/Typography'
-import DividerWithText from './Divider'
-import {PopupButton,FbButton,GoogleButton} from './Button'
+import Box from '@material-ui/core/Box'
+import SwipeableViews from 'react-swipeable-views'
+import RegistrationForm from './RegistrationForm'
+import EmployeeRegistrationForm from './Employee'
 
+// import AppBar from '@material-ui/core/AppBar'
+// import Tabs from '@material-ui/core/Tabs'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={2}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  }
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 443,
+  },
+}))
 
 export default function FormDialog() {
     const [open, setOpen] = React.useState(false)
@@ -21,87 +68,22 @@ export default function FormDialog() {
     const handleClose = () => {
       setOpen(false)
     }
-const useStyles = makeStyles((theme) => ({
-  [theme.breakpoints.down('sm')]: {
-    textAlign:'center',
-  },
-  [theme.breakpoints.between('sm','md')]: {
-    textAlign:'center',
-  },
-  [theme.breakpoints.up('md')]: {
-    textAlign:'center',
-  },
-  title:{
-    padding: '0px 30px',
-    paddingTop: '2vh',
-    fontWeight:'bold',
-  },
-  textField: {
-    [`& fieldset`]: {
-      borderRadius: '30px',
-    },
-    width:'85%',
-    marginBottom:10,
-  },
-  text:{
-    fontWeight:'bold',
-    width:'85%',
-    marginLeft:'8%'
-  },
-  mention:{
-    fontWeight: 'bold',
-    color: '#007bf5',
-    float: 'right',
-    fontSize:'12px',
-    textDecoration: 'none',
-    marginRight:'8%',
-    marginBottom:'15px'
-  },
-  divider:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-  },
-  agreement:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-    marginLeft:'34px',
-    width:'100%',
-    marginBottom:'5px' 
-  },
-  divide:{
-    borderBottom: "1px solid black",
-    width:'85%',
-    marginBottom:10
-  },
-  account:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-    marginLeft:'34px',
-    marginBottom:'-22px'
-  },
-  login:{
-    fontWeight: 'bold',
-    color: '#007bf5',
-    // float: 'right',
-    fontSize:'12px',
-    textDecoration: 'none',
-    marginRight:'10%',
-    marginBottom:'30px'
-  },
-  loginColor:{
-    textDecoration: 'none',
-    color: '#007bf5',
-  }
- 
-}))
-
-const classes = useStyles()
+    const theme = useTheme()
+    const classes = useStyles()
+    const [value, setValue] = React.useState(0)
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue)
+    }
+  
+    const handleChangeIndex = (index) => {
+      setValue(index)
+    }
     return (
       <>
         <Button onClick={handleClickOpen}>
           Sign up
-        </Button>  
-             
+        </Button>
         <Dialog
           open={open}
           onClose={handleClose}
@@ -109,126 +91,38 @@ const classes = useStyles()
           fullWidth
           maxWidth='xs'
         >
-          <Grid container justify="center">
-            <DialogTitle className="Dialog-title">
-              <Typography
-                className={classes.title}
-                align="center"
-                variant="h4"
+          <div className={classes.root}>
+            <AppBar position="static" color="default">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant="fullWidth"
+                aria-label="full width tabs example"
               >
-                Join us
-              </Typography>
-            </DialogTitle>
-          </Grid>
-          <Grid container>
-            <Typography
-              className={classes.text}
+                <Tab label="Sign up as customer" {...a11yProps(0)} />
+                <Tab label="Sign up as employee" {...a11yProps(1)} />
+              </Tabs>
+            </AppBar>
+            <SwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              index={value}
+              onChangeIndex={handleChangeIndex}
             >
-              Email
-            </Typography>
-          </Grid>
-          <Grid container justify="center">
-            <TextField
-              className={classes.textField}
-              margin="dense"
-              id="outlined-basic"
-              label="Email"
-              type="Email"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid>
-            <Typography
-              className={classes.text}
-            >
-              Password
-            </Typography>
-          </Grid>
-          <Grid container justify="center">
-            <TextField
-              className={classes.textField}
-              margin="dense"
-              id="outlined-basic"
-              label="Password"
-              type="password"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid>
-            <a href="/password">
-              <Typography
-                className={classes.mention}
-              >
-                Forgot Password?
-              </Typography>
-            </a>
-          </Grid>
-            
-          <Grid container justify="center">
-            <PopupButton />
-            
-          </Grid>
-          <Grid container justify="center">
-            <DividerWithText>
-              <Typography
-                className={classes.divider}
-              >
-                or sign up with
-              </Typography>
-            </DividerWithText>
-          </Grid>
-           
-          <Grid container justify="center" item xs={12}>
-            <FbButton />
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <GoogleButton />
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <Typography
-              className={classes.agreement}
-            >
-              By signing up, I agree to terms & conditions.
-            </Typography>
-              
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <Divider className={classes.divide} />
-          </Grid>
-            
-          <Grid container direction="row">
-            <Grid container justify="flex-start">
-              <Typography className={classes.account}>
-                Do not have an account?
-              </Typography>
-              
-            </Grid>
-            <Grid container justify="flex-end">
-               
-              <Typography className={classes.login}>
-                <a href="/login" className={classes.loginColor}>
-                  Login
-                </a>
-              </Typography>
-               
-            </Grid>
-          </Grid>
+              <TabPanel value={value} index={0} dir={theme.direction}>
+                <RegistrationForm />
+              </TabPanel>
+              <TabPanel value={value} index={1} dir={theme.direction}>
+                <EmployeeRegistrationForm />
+              </TabPanel>
+            </SwipeableViews>
+          </div>
         </Dialog>
        
       </>
     )
   }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src/components/SignUpComponents/PopupFormLoginMenu.js
+++ src/components/SignUpComponents/PopupFormLoginMenu.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import Divider from '@material-ui/core/Divider'
-import {makeStyles} from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
 import {MenuItem} from '@material-ui/core'
-import DividerWithText from './Divider'
-import {FbButton,GoogleButton,PopupLoginButton} from './Button'
-
+import LoginDetails from './LoginForm'
 
 export default function FormDialogMenuLogin() {
-    const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false)
   
     const handleClickOpen = () => {
       setOpen(true)
@@ -21,86 +13,13 @@ export default function FormDialogMenuLogin() {
     const handleClose = () => {
       setOpen(false)
     }
-const useStyles = makeStyles((theme) => ({
-  [theme.breakpoints.down('sm')]: {
-    textAlign:'center',
-  },
-  [theme.breakpoints.between('sm','md')]: {
-    textAlign:'center',
-  },
-  [theme.breakpoints.up('md')]: {
-    textAlign:'center',
-  },
-  title:{
-    padding: '0px 30px',
-    paddingTop: '2vh',
-    fontWeight:'bold',
-  },
-  textField: {
-    [`& fieldset`]: {
-      borderRadius: '30px',
-    },
-    width:'85%',
-    marginBottom:10,
-  },
-  text:{
-    fontWeight:'bold',
-    width:'85%',
-    marginLeft:'8%'
-  },
-  mention:{
-    fontWeight: 'bold',
-    color: '#007bf5',
-    float: 'right',
-    fontSize:'12px',
-    textDecoration: 'none',
-    marginRight:'8%',
-    marginBottom:'15px'
-  },
-  divider:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-  },
-  agreement:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-    marginLeft:'34px',
-    width:'100%',
-    marginBottom:'5px' 
-  },
-  divide:{
-    borderBottom: "1px solid black",
-    width:'85%',
-    marginBottom:10
-  },
-  account:{
-    fontWeight: 'bold',
-    fontSize:'12px',
-    marginLeft:'34px',
-    marginBottom:'-22px'
-  },
-  login:{
-    fontWeight: 'bold',
-    color: '#007bf5',
-    // float: 'right',
-    fontSize:'12px',
-    textDecoration: 'none',
-    marginRight:'10%',
-    marginBottom:'30px'
-  },
-  loginColor:{
-    textDecoration: 'none',
-    color: '#007bf5',
-  }
- 
-}))
+    
 
-const classes = useStyles()
     return (
       <>
         <MenuItem onClick={handleClickOpen}>
           Login
-        </MenuItem>       
+        </MenuItem>
         <Dialog
           open={open}
           onClose={handleClose}
@@ -108,114 +27,8 @@ const classes = useStyles()
           fullWidth
           maxWidth='xs'
         >
-          <Grid container justify="center">
-            <DialogTitle className="Dialog-title">
-              <Typography
-                className={classes.title}
-                align="center"
-                variant="h4"
-              >
-                Login
-              </Typography>
-            </DialogTitle>
-          </Grid>
-          <Grid container>
-            <Typography
-              className={classes.text}
-            >
-              Email
-            </Typography>
-          </Grid>
-          <Grid container justify="center">
-            <TextField
-              className={classes.textField}
-              margin="dense"
-              id="outlined-basic"
-              label="Email"
-              type="Email"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid>
-            <Typography
-              className={classes.text}
-            >
-              Password
-            </Typography>
-          </Grid>
-          <Grid container justify="center">
-            <TextField
-              className={classes.textField}
-              margin="dense"
-              id="outlined-basic"
-              label="Password"
-              type="password"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid>
-            <a href="/password">
-              <Typography
-                className={classes.mention}
-              >
-                Forgot Password?
-              </Typography>
-            </a>
-          </Grid>
-            
-          <Grid container justify="center">
-            <PopupLoginButton />
-          </Grid>
-          <Grid container justify="center">
-            <DividerWithText>
-              <Typography
-                className={classes.divider}
-              >
-                or login with
-              </Typography>
-            </DividerWithText>
-          </Grid>
-           
-          <Grid container justify="center" item xs={12}>
-            <FbButton />
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <GoogleButton />
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <Typography
-              className={classes.agreement}
-            >
-              By logging in, I agree to terms & conditions.
-            </Typography>
-              
-          </Grid>
-          <Grid container justify="center" item xs={12}>
-            <Divider className={classes.divide} />
-          </Grid>
-            
-          <Grid container direction="row">
-            <Grid container justify="flex-start">
-              <Typography className={classes.account}>
-                Do not have an account?
-              </Typography>
-              
-            </Grid>
-            <Grid container justify="flex-end">
-               
-              <Typography className={classes.login}>
-                <a href="/login" className={classes.loginColor}>
-                  Sign Up
-                </a>
-              </Typography>
-               
-            </Grid>
-          </Grid>
+          <LoginDetails />
         </Dialog>
       </>
     )
-  }
-
-
-
-
+    }

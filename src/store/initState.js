95c0ca6e7f2729af03f2a7268b98788f0c55b077
Vincent <415578597@qq.com>
2021-03-28 22:28:30 +1000

    rename regular-saga

---
+++ src/store/initState.js
@@ -0,0 +1,26 @@
+// const initialState = {
+//   order: {
+//     loading: false,
+//     error:null,
+//     repos_in_reducer_init: 'init value',
+//     completeinfo:{
+//       info: localStorage.getItem('regularCleanOrder')? 
+//         JSON.parse(localStorage.getItem('regularCleanOrder')):{
+//           bedroomNum:'',
+//           bathroomNum:'',
+//           type:'',
+//           address:{
+//             address1:'',
+//             address2:'',
+//             suburb:'',
+//             state:'',
+//             postcode:''
+//           },
+//           startTime:'',
+//           price:0,
+//         }
+//     }
+//   }
+// }
+
+// export default initialState

d4bb7e9ee03bb1d75f6e2a76c9a5cad241f9129f
Kathy <katekathywu@gmail.com>
2021-03-30 22:52:06 +1100

    update customer list & staffs list edit button

--- src/components/AdminComponents/ListTableRow.js
+++ src/components/AdminComponents/ListTableRow.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-alert */
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { TableRow, TableCell, Button, Avatar, makeStyles } from '@material-ui/core/'
 import { GreenStatus, RedStatus } from '../UIComponents/Status'
 
@@ -15,9 +16,10 @@ const useStyle = makeStyles((theme) => ({
   },
 }))
 
-function CheckUser(id) {
-  alert(`Check UserID: ${id}`)
-}
+// function CheckUser(id) {
+//   const path = `/admin/customers/?id=${id}`
+//   <Link to=path />
+// }
 
 function DeleteUser(id) {
   alert(`Delete UserID: ${id}`)
@@ -32,10 +34,10 @@ function StatusDisplay(tableType, status) {
     (<TableCell align="center"><GreenStatus>Avalilable</GreenStatus></TableCell>) : 
     (<TableCell align="center"><RedStatus>Unavailable</RedStatus></TableCell>)
 }
-
 function ListTableRow(props) {
   const classes = useStyle()
   const { id, firstName, lastName, status, ongoingOrder, completedOrder, tableType } = props
+  const path = `/admin/${tableType}s/${id}`
   return (
     <TableRow role="checkbox" tabIndex={-1} key={id}>
       <TableCell align="center">
@@ -66,7 +68,11 @@ function ListTableRow(props) {
           variant="contained"
           color="primary"
           className={classes.actionBtn}
-          onClick={() => CheckUser(id)}
+          // componet={Link}
+          // to="/fheawljfbel"
+          // onClick={() => CheckUser(id)}
+          component={Link} 
+          to={path}
         >
           Check
         </Button>

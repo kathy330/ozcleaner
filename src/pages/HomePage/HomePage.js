14fea4ebabceca6773d883c478499a1ce6e69308
Vincent <415578597@qq.com>
2021-04-02 23:50:46 +1000

    删除authlevel赋值

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -20,7 +20,8 @@ function Home() {
   const trigger = useScrollTrigger({ disableHysteresis: true })
   
   // localStorage.setItem("authLevel", "") // 如果为空，则不能进入order 页面
-  localStorage.setItem("authLevel", "user") // 可以进入order页面
+  // localStorage.setItem("authLevel", "user") // 可以进入order页面
+  // localStorage.setItem("authLevel", "employee") // 可以进入employee,order页面
   // localStorage.setItem("authLevel", "admin") // 可以进入admin,order页面
   
   return (

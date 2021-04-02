76b5a02c9b9ef2f89d817e2f43e111dafe2eb81b
Wystanzeng <53050221+Wystanzeng@users.noreply.github.com>
2021-04-02 18:10:13 +1000

    update

--- src/pages/AdminPage/AdminIndexPage.js
+++ src/pages/AdminPage/AdminIndexPage.js
@@ -1,16 +1,13 @@
 import React from "react"
 
-import {Switch} from 'react-router'
+
 import AdminHeaderNavigation from "../../components/AdminComponents/Dashboard/Navigation"
 import AdminTabs from "../../components/AdminComponents/Dashboard/Tabs"
-import ProtectedRoute from "../../router/ProtectedRoute"
-import AdminDashboardPage from "./AdminDashboardPage"
-import AdminCustomersListPage from "./AdminCustomersListPage"
-import AdminStaffsListPage from "./AdminStaffsListPage"
 import Footer from '../../components/FooterComponents/Footer'
 
 
 function AdminIndexPage(){
+  
     return (
       <div>
         {/* <NavBar /> */}
@@ -20,11 +17,6 @@ function AdminIndexPage(){
         <AdminTabs />
         {/* <footer /> */}
 
-        <Switch>
-          <ProtectedRoute path="/dashboard" component={AdminDashboardPage} />
-          <ProtectedRoute path="/customers" component={AdminCustomersListPage} />
-          <ProtectedRoute path="/staffs" component={AdminStaffsListPage} />
-        </Switch>
 
         <Footer />
       </div>

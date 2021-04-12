e66ecffcf9bf861645ef87d6aae0f577b7a4b987
Kathy <katekathywu@gmail.com>
2021-04-12 22:07:15 +1000

    add comment

---
+++ src/components/AdminComponents/DialogPopup.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core'
+
+/**
+ * DialogPopup() is a pagination components
+ * @param open: (bool) set dialog to open
+ * @param handleAlertClose: (func) handle close the dialog
+ * @param handleAlertConfirm: (func) handle confirm action for the dialog
+ */
+export default function DialogPopup (props) {
+  const { open, handleAlertClose, handleAlertConfirm, userdata } = props
+  return(
+    <Dialog
+      open={open}
+      onClose={handleAlertClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">Do you want to delete this user?</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {userdata && `User: ${userdata.name.firstName}(${userdata.email})`}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleAlertConfirm} variant="contained" color="secondary">
+          Delete
+        </Button>
+        <Button onClick={handleAlertClose} variant="contained" autoFocus>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
